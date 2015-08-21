f5736cbcb56b12f274038e3dd3373632bae1245c
jestertaguibao <taguibaojester@gmail.com>
2015-08-21 16:43:23 +0800

    template system

--- html/foot.php
+++ html/foot.php
@@ -1,4 +1,3 @@
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<?php include('cdn/cdnjs.php'); ?>	
 </body>
 </html>
--- html/head.php
+++ html/head.php
@@ -4,11 +4,6 @@
 	<title>
 	Project
 	</title>
-
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-	
+<?php include('cdn/cdncss.php'); ?>	
 </head>
 <body>
